ce1350d8db57a8443c6c3a61b6d3fb3b577f5276
Guiu <guiur@hotmail.com>
2023-11-22 18:51:25 +0100

    Prepared basic structure

---
+++ La Guia Completa/follow-up-react/src/components/Form.jsx
@@ -0,0 +1,10 @@
+function Form() {
+
+    return(
+        <>
+            <h2>Follow up Form</h2>
+        </>
+    )
+}
+
+export default Form;
---
+++ La Guia Completa/follow-up-react/src/components/ListPatients.jsx
@@ -0,0 +1,9 @@
+function ListPatients() {
+  return (
+    <>
+       <h3>Patients List</h3> 
+    </>
+  )
+}
+
+export default ListPatients
